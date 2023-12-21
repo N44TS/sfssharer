a6e1dce503bb2fb538e8d08f7816bdcd2202a024
Natalie <natalieg@Natalies-MacBook-Air-3.local>
2023-12-21 09:02:06 +0000

    new look

--- src/App.js
+++ src/App.js
@@ -13,6 +13,15 @@ function App() {
   const [winningNumber, setWinningNumber] = useState("");
   const [sfsFeeAmount, setSfsFeeAmount] = useState("");
   const [isWinner, setIsWinner] = useState(false);
+  const [timeLeft, setTimeLeft] = useState({
+    days: "00",
+    hours: "00",
+    minutes: "00",
+  });
+
+  // Temporary variable for development (set to 'true' to simulate being a winner)
+  const [isTempWinner, setIsTempWinner] = useState(false); // Change to true to test winner view
+
 
   useEffect(() => {
     const init = async () => {
@@ -104,66 +113,95 @@ function App() {
     }
   };
 
+  // Temporary!!! function to toggle winner status (for development)
+  const toggleWinnerStatus = () => {
+    setIsTempWinner(!isTempWinner);
+  };
+
+  // Handle claim reward // STILL NEEDS DOING!!
+  // const handleClaimReward = () => {
+  //   // Add logic to claim the reward
+  //   console.log("Reward claimed");
+  //   setShowWinningAnimation(false); // Hide animation after claiming
+  // };
+
   return (
     <div className="app-container">
-      <h1 className="header">GumbleDapp</h1>
-      <h2 className="description">
-        Predict the Milady NFT floor price - win rewards!
-      </h2>
-      <h4>
-        rules: cloest to actual price after 2 weeks wins a share of the contract
-        fees(SFS)! . The more people interact with this contract, the more Eth
-        to be won!
-      </h4>
+      {/* Temporary!!! button to toggle winner status */}
+    <button onClick={toggleWinnerStatus}>Toggle Winner Status (Dev)</button>
+           {/* {isWinner && (
+            <section className="claim-reward">
+              <button onClick={claimReward} className="claim-button">
+                Claim Your Reward
+              </button>
+            </section>
+          )} */}
+       {/* Check if user is a winner */}
+       {isTempWinner && (
+       <div className="winning-animation">
+       <div className="winning-message">You Won!
+       <p>yes, srsly....</p>
+       <button onClick={claimReward} className="claim-button">get my money</button>
+    </div>
+     </div>
+      )}
 
-      {/* Prediction Input */}
-      <div className="section">
-        <input
-          type="number"
-          value={prediction}
-          className="input-field"
-          onChange={(e) => setPrediction(e.target.value)}
-          placeholder="Your Prediction"
-        />
-        <button onClick={submitPrediction} className="button">
-          Submit
-        </button>
+      <div className="wallet-address">
+        {account && (
+          <p>
+            Connected: {account.substring(0, 4)}...
+            {account.substring(account.length - 5)}
+          </p>
+        )}
+      </div>
+      <div className="gamble-box">
+        <header className="app-header">
+          <h1 className="app-title">Gumbledapp</h1>
+          <div className="countdown-timer">
+            <p>Time left to play</p>
+            <span className="timer-days">00</span>d :
+            <span className="timer-hours">00</span>h :
+            <span className="timer-minutes">00</span>m
+          </div>
+          {isAdmin && <div className="admin-button">Admin</div>}
+        </header>
+
+        <main className="main-content">
+          <div className="prediction-section">
+            <h2 className="section-title">This Week's Gumble</h2>
+            <div className="prediction-input-wrapper">
+              <input
+                type="number"
+                value={prediction}
+                onChange={(e) => setPrediction(e.target.value)}
+                className="prediction-input"
+                placeholder="predict MILADY floor price on nye"
+              />
+              <button onClick={submitPrediction} className="submit-button">
+                Submit
+              </button>
+            </div>
+            {/* *pay gas only */}
+          </div> 
+
+          {isAdmin && (
+            <section className="admin-panel">
+              {/* Admin panel inputs and buttons */}
+            </section>
+          )}
+        </main>
       </div>
 
-      {/* Claim Reward */}
-      {isWinner && (
-        <div className="section">
-          <button onClick={claimReward} className="button">
-            Claim Reward
-          </button>
+      <div className="rules-section">
+        <div className="rules">
+          <h3 className="rules-title">
+            Rules: closest to actual price after 2 weeks wins a share of the
+            contract fees(SFS)! . The more people interact with this contract,
+            the more Eth to be won!
+          </h3>
+          {/* Rules text here */}
         </div>
-      )}
-
-      {/* Admin Panel */}
-      {isAdmin && (
-        <div className="section">
-          <input
-            type="number"
-            value={winningNumber}
-            className="input-field"
-            onChange={(e) => setWinningNumber(e.target.value)}
-            placeholder="Set Winning Number"
-          />
-          <button onClick={setWinningNum} className="button">
-            Set Winning Number
-          </button>
-          <input
-            type="number"
-            value={sfsFeeAmount}
-            className="input-field"
-            onChange={(e) => setSfsFeeAmount(e.target.value)}
-            placeholder="SFS Fee Amount"
-          />
-          <button onClick={claimSFSFees} className="button">
-            Claim SFS Fees
-          </button>
-        </div>
-      )}
+      </div>
     </div>
   );
 }
